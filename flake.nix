9783e935ebdbd6edcb413942c8f06f2249030911
Dylan <boss@tehbox.org>
2026-03-27 20:48:19 +1300

    feat: Updated nixos version
    Going from nixos-25.05 to nixos-25.11 (new stable version)

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
     description = "STK-server";
     inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     };
     outputs = { self, nixpkgs, ... }@inputs:
         let pkgs = nixpkgs.legacyPackages.x86_64-linux;
